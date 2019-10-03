434b1468ea5a5c8b61cdb12906595c3ae371ae23
Michael R.F <mrflowers22@uwalumni.com>
2019-10-03 12:42:40 -0600

    constructed the model so I can parse it later

---
+++ MovieSearch/Model/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Michael Flowers on 10/3/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+//// apikey:  3021207a0f44385e84ef7cc905fb9320
+///  baseURL: https://api.themoviedb.org/3/search/movie?api_key=3021207a0f44385e84ef7cc905fb9320&query=Jack+Reacher
+struct MovieSearch: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let title: String
+    let rating: String
+    let summary: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case rating = "vote_average"
+        case summary = "overview"
+    }
+}
